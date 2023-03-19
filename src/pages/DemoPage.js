9f6d810ec265d80f5c5328b4afba302b4586b200
danmaevsky <dan.mayevskiy@gmail.com>
2023-03-18 21:44:19 -0400

    Added gh-pages stuff to package.json

--- src/pages/DemoPage.js
+++ src/pages/DemoPage.js
@@ -168,19 +168,9 @@ function SearchResult(props) {
         return (
             <li className="demo-page-search-result">
                 <div>
-                    <h5>
-                        {result.name
-                            .toLowerCase()
-                            .split(" ")
-                            .map((s) => {
-                                if (s === "bbq") {
-                                    return "BBQ";
-                                }
-                                return s.charAt(0).toUpperCase() + s.substring(1);
-                            })
-                            .join(" ")}
-                    </h5>
-                    {result.brandName ? <p>{result.brandName}</p> : null}
+                    <h5>{ProcessFoodName(result.name)}</h5>
+                    <p>Original Text: {result.name} </p>
+                    {result.brandName ? <p>{ToTitleCase(result.brandName)}</p> : null}
                 </div>
             </li>
         );
@@ -306,11 +296,132 @@ function SearchBox(props) {
                         onSubmit(textValue);
                         return;
                     }
-                }} 
+                }}
             ></input>
             <button className="demo-page-search-box-submit" onClick={() => onSubmit(textValue)}></button>
         </div>
     );
+}
+
+/* Utility Functions */
+// Food Search Utilities
+function ToTitleCase(x) {
+    x = x
+        .toLowerCase()
+        .split(" ")
+        .map((s) => {
+            if (s === "bbq") {
+                return "BBQ";
+            }
+            return s.charAt(0).toUpperCase() + s.substring(1);
+        })
+        .join(" ");
+    // extra capitalization
+    for (let i = 0; i < x.length - 1; i++) {
+        if (x[i] === "(" || x[i] === "[" || x[i] === "{" || x[i] === "-") {
+            x = x.substring(0, i + 1) + x[i + 1].toUpperCase() + x.substring(i + 2);
+        }
+    }
+    return x;
+}
+function GetPhrases(s, ans, offset) {
+    ans = ans ? ans : {};
+    let j = offset ? offset : 0;
+    let k = offset ? offset : 0;
+    let builder = "";
+    for (let i = offset ? offset : 0; i < s.length; i++) {
+        // !offset ? console.log(builder) : null;
+        if (builder === "") {
+            j = i;
+            k = i;
+        }
+        if (s[i] === " ") {
+            // console.log("space", builder);
+            if (ans[builder]) {
+                if (!MultiDimIncludes(ans[builder], [j, k])) {
+                    ans[builder].push([j, k]);
+                }
+            } else {
+                ans[builder] = [[j, k]];
+                GetPhrases(s, ans, i + 1);
+            }
+            builder = builder === "" ? "" : builder + s[i];
+            k++;
+        } else if (s[i] === ",") {
+            // console.log("comma", builder);
+            if (ans[builder]) {
+                if (!MultiDimIncludes(ans[builder], [j, k])) {
+                    ans[builder].push([j, k]);
+                }
+            } else {
+                ans[builder] = [[j, k]];
+                GetPhrases(s, ans, i + 1);
+            }
+            builder = "";
+        } else if (i === s.length - 1) {
+            builder += s[i];
+            k++;
+            // console.log("end", builder);
+            if (ans[builder]) {
+                if (!MultiDimIncludes(ans[builder], [j, k])) {
+                    ans[builder].push([j, k]);
+                }
+            } else {
+                ans[builder] = [[j, k]];
+            }
+        } else {
+            builder += s[i];
+            k++;
+        }
+    }
+    return ans;
+}
+
+function MultiDimIncludes(arr, val) {
+    for (let i = 0; i < arr.length; i++) {
+        let temp = true;
+        for (let j = 0; j < arr[i].length; j++) {
+            temp = arr[i][j] === val[j];
+        }
+        if (temp) {
+            return temp;
+        }
+    }
+    return false;
+}
+
+export function ProcessFoodName(x) {
+    let phrases = GetPhrases(x);
+    // console.log(phrases);
+    let maxKey = "";
+    let keys = Object.keys(phrases);
+    for (let i = 0; i < keys.length; i++) {
+        let key = keys[i];
+        if (key.length > maxKey.length && phrases[key].length > 1) {
+            maxKey = key;
+        }
+    }
+
+    if (maxKey !== "") {
+        // console.log(maxKey);
+        phrases[maxKey].sort((a, b) => {
+            if (a[0] < b[0]) {
+                return -1;
+            }
+            if (a[0] > b[0]) {
+                return 1;
+            }
+            return 0;
+        });
+        x = x.substring(0, phrases[maxKey][phrases[maxKey].length - 1][0]) + x.substring(phrases[maxKey][phrases[maxKey].length - 1][1]);
+        x = x.replace("  ", " ");
+        if (x.substring(x.length - 2) === ", ") {
+            x = x.substring(0, x.length - 2);
+        }
+    }
+    // to Title Case
+    x = ToTitleCase(x);
+    return x;
 }
 
 export default DemoPage;
